38b870381363cb2048764cc11d494043dacfacf8
chenxingqi <1203020266@qq.com>
2013-07-23 10:13:49 +0800

    add depot5

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,8 @@
 class StoreController < ApplicationController
+  skip_before_filter :authorize
+  
   def index
 	@products = Product.order(:title)#books are ordered by title
+	@cart = current_cart
   end
 end
