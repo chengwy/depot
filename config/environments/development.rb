6c8fdc70d426696ed5da8e8fd138056defa7fd91
smy123 <952975674@qq.com>
2013-07-26 15:46:03 +0800

    depot4 sunmaoyu

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,6 @@
 Depot::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
-#test
+
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
@@ -14,8 +14,7 @@ Depot::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send
-  config.action_mailer.raise_delivery_errors = false
-  
+ config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :smtp
      config.action_mailer.smtp_settings = {
        address:        "smtp.gmail.com",
