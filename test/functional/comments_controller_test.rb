5c336c00c7d3e68c0092ae6485785e0850351c4c
yumeiling <yumling@mail2.sysu.edu.cn>
2013-08-06 10:20:19 +0800

    new

--- test/functional/comments_controller_test.rb
+++ test/functional/comments_controller_test.rb
@@ -1,9 +1,14 @@
 require 'test_helper'
 
 class CommentsControllerTest < ActionController::TestCase
-  test "should get create" do
-    get :create
-    assert_response :success
+  test "should create comment" do
+    assert_difference('Comment.count') do
+      #modified ----------------------------------------
+      post :create, comment: @update
+      #modified ----------------------------------------
+    end
+
+    assert_redirected_to product_path(assigns(:comment))
   end
 
   test "should get destroy" do
