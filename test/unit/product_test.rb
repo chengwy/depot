eb2109d340e30ff9e070dcadd34a7b7cc64cbdc2
yumeiling <yumling@mail2.sysu.edu.cn>
2013-08-07 15:01:09 +0800

    final

--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -43,7 +43,7 @@ class ProductTest < ActiveSupport::TestCase
     assert_equal "must be greater than or equal to 0.01",
       product.errors[:price].join('; ')
     product.price = 1
-    assert product.valid?
+    assert product.valid?,"invalid"
   end
   def new_product (image_url)
     product = Product.new(title:       "My Book Title",
